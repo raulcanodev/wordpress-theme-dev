2816cf4652a2ab9aa6c67a9a82ae0a20ff38de2c
Raul Marco Cano <raul@widgilabs.com>
2024-05-26 19:45:06 +0100

    new theme

---
+++ themes/wp-devs/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset=<?php bloginfo('charset'); ?>>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body <?php body_class(); ?>>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus voluptatem provident molestias alias rem quaerat dolore nihil, illo sunt saepe repellat, dolores harum quam vitae quod magnam corporis aspernatur obcaecati!</p>
+  <div id="page" class="site">
+    <header>
+      <section class="top-bar">
+        <div class="logo">
+          Logo
+        </div>
+        <div class="searchbox">
+          Search
+        </div>
+      </section>
+      <section class="menu-area">
+        <nav class="main-menu">
+          Menu
+        </nav>
+      </section>
+    </header>
--- themes/wp-devs/index.php
+++ themes/wp-devs/index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+    <? get_header(); ?>
+    <!--  -->
+    <div id="content" class="site-content">
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+          <section class="hero">
+            Hero
+          </section>
+          <section class="services">
+            Services
+          </section>
+          <section class="home-blog">
+            Blog
+          </section>
+        </main>
+      </div>
+    </div>
+    <!--  -->
+    <? get_footer(); ?>
